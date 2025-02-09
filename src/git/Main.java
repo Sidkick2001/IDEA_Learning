b38c15127f205cbc4591caf91cfe4fa2626ce690
sidjeoge <dimasecuriti@gmail.com>
2025-02-09 12:46:30 +0300

    Edit Main.java

--- src/git/Main.java
+++ src/git/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int primetiveInt = 5;
         Integer objectInt = primetiveInt;
-
         System.out.println(objectInt);
+
+        System.out.println("1");
+        String str = "Lol";
 
 
 
