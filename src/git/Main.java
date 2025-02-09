9481fb03f58fbd07b7afd42af9f21f5f0a4afb1e
sidjeoge <dimasecuriti@gmail.com>
2025-02-09 12:17:17 +0300

    Init

---
+++ src/git/Main.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Main {
+    public static void main(String[] args) {
+        int primetiveInt = 5;
+        Integer objectInt = primetiveInt;
+
+        System.out.println(objectInt);
+
+
+
+    }
+}
